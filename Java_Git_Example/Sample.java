e1e4deb9d3467a7cf4812831fec8fa5884f666fa
sameersriram <sameerananthsriram@gmail.com>
2021-04-09 18:59:38 +0530

    slave merged

--- Java_Git_Example/Sample.java
+++ Java_Git_Example/Sample.java
@@ -1,10 +1,6 @@
 package Java_Git_Example;
 public class Sample{
     public static void main(String args[]){
-<<<<<<< HEAD
         System.out.println("Hello World Java");
-=======
-        System.out.println("Hello World Guys");
->>>>>>> master
     }
 }
